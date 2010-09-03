daf9bed4ec0153244668b89c032613b94df3abbc
Iiro Uusitalo <iiro.uusitalo@samk.fi>
2010-09-03 05:37:39 -0700

    Small changes

--- scripts/massidea_install.sh
+++ scripts/massidea_install.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+!/bin/bash
 
 ## logien luominen
 touch $HOME/massidea/logs/register.log
 touch $HOME/massidea/logs/errors.log
 touch $HOME/massidea/logs/contentpublish.log
-chmod 777 $HOME/massidea/logs/* $HOME/massidea/tmp $HOME/massidea/www/upload
+chmod 777 $HOME/massidea/logs/* $HOME/massidea/tmp $HOME/massidea/www/upload $HOME/massidea/www/files $HOME/massidea/www/temp
 
 echo -e "Created logs!"
 
@@ -15,6 +15,11 @@ else
         echo -e "RewriteBase /~`echo $USER`/" >> $HOME/massidea/www/.htaccess
         echo "Added RewriteBase! (modified: `echo $HOME/massidea/www/.htaccess`)"
         cd $HOME/massidea/
-        /usr/bin/git update-index --assume-unchanged logs/*
-        /usr/bin/git update-index --assume-unchanged www/.htaccess
+        /usr/bin/git update-index --assume-unchanged $HOME/massidea/config/
+        /usr/bin/git update-index --assume-unchanged $HOME/massidea/logs/
+        /usr/bin/git update-index --assume-unchanged $HOME/massidea/www/.htaccess
+        /usr/bin/git update-index --assume-unchanged $HOME/massidea/tmp/
+        /usr/bin/git update-index --assume-unchanged $HOME/massidea/www/upload/
+        /usr/bin/git update-index --assume-unchanged $HOME/massidea/www/files/
+        /usr/bin/git update-index --assume-unchanged $HOME/massidea/www/temp/
 fi
